7f02f221fb9029002c1c9680cad6df61611d3aad
Priyanshu Joshi <joshipriyanshu2029@gmail.com>
2025-06-22 19:44:17 +0530

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://priyanshujoshi99.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/knowledge-hub',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
